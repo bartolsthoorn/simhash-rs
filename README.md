d9bd03706a19e2f9f8615dd25f90518e278da95e
Bart Olsthoorn <bartolsthoorn@gmail.com>
2014-08-12 17:27:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simhash for Rust
+![travis-ci status](https://travis-ci.org/bartolsthoorn/rust-simhash.svg?branch=master)
+Simhash algorithm (developed by [Moses Charikar](http://www.cs.princeton.edu/courses/archive/spring04/cos598B/bib/CharikarEstim.pdf)) implemented in Rust
+
+~~~rust
+extern crate simhash;
+// now use simhash::hash
+~~~
+
+### TODO
+- Calculate hamming distance / similarity
+- (Optional) Support 32 bit hashing instead of 64
---
+++ src/simhash.rs
@@ -0,0 +1,41 @@
+// Rust Simhash
+// Implemented by Bart Olsthoorn on 12/08/2014
+// With the help of http://matpalm.com/resemblance/simhash/
+
+#![license = "MIT"]
+#![crate_type = "lib"]
+
+pub mod simhash {
+  use std::hash;
+
+  pub fn hash(subject: &str) -> u64 {
+    let mut v: [int, ..64]  = [0, ..64];
+    let mut simhash: u64 = 0;
+
+    for feature in subject.words() {
+      let feature_hash: u64 = hash::hash(&feature);
+
+      for i in range(0, 64) {
+        let bit = (feature_hash >> i) & 1;
+        if bit == 1 {
+          v[i] = v[i] + 1;
+        } else {
+          v[i] = v[i] - 1;
+        }
+      }
+    }
+
+    for q in range(0, 64) {
+      if v[q] > 0 {
+        simhash |= 1 << q;
+      }
+    }
+    simhash
+  }
+}
+
+#[test]
+fn simhash_test() {
+  assert!(simhash::hash("The cat sat on the mat") == 2595200813813010837);
+  assert!(simhash::hash("Why the lucky stiff") == 1155526875459215761);
+}
