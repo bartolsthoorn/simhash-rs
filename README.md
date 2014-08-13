4dddf861a0de3265de83b7fb8bd1e90dd0b4549b
Bart Olsthoorn <bartolsthoorn@gmail.com>
2014-08-13 02:19:12 +0200

    Add hamming distance and similarity functions

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Simhash for Rust
 ![travis-ci status](https://travis-ci.org/bartolsthoorn/rust-simhash.svg?branch=master)
 
-Simhash algorithm (developed by [Moses Charikar](http://www.cs.princeton.edu/courses/archive/spring04/cos598B/bib/CharikarEstim.pdf)) implemented in Rust
+Simhash algorithm (developed by [Moses Charikar](http://www.cs.princeton.edu/courses/archive/spring04/cos598B/bib/CharikarEstim.pdf)) implemented in Rust. It generates 64 bit simhashes and can calculate similarities of two subjects using the [Hamming distance](http://en.wikipedia.org/wiki/Hamming_distance).
 
 ~~~rust
 extern crate simhash;
-// now use simhash::hash
-~~~
 
-### TODO
-- Calculate hamming distance / similarity
-- (Optional) Support 32 bit hashing instead of 64
+// use simhash::hash("This is an example sentence") to simhash a sentence
+// use simhash::similarity("Sentence A", "Sentence B") to get a 0.0 - 1.0 similarity score
+
+// check src/lib.rs for hamming_distance and hash_similarity functions if you need those
+~~~
--- src/lib.rs
+++ src/lib.rs
@@ -31,11 +31,45 @@ pub mod simhash {
     }
     simhash
   }
+
+  pub fn hamming_distance(x: u64, y: u64) -> u64 {
+    (x ^ y).count_ones()
+  }
+
+  pub fn hash_similarity(hash1: u64, hash2: u64) -> f64 {
+    let distance: f64 = hamming_distance(hash1, hash2) as f64;
+    1.0 - (distance / 64.0)
+  }
+
+  pub fn similarity(x: &str, y: &str) -> f64 {
+    hash_similarity(hash(x), hash(y))
+  }
 }
 
 #[test]
 fn simhash_test() {
-  assert!(simhash::hash("The cat sat on the mat") == 2595200813813010837);
-  assert!(simhash::hash("The cat sat under the mat") == 2595269945604666783);
-  assert!(simhash::hash("Why the lucky stiff") == 1155526875459215761);
+  assert_eq!(simhash::hash("The cat sat on the mat"), 2595200813813010837);
+  assert_eq!(simhash::hash("The cat sat under the mat"), 2595269945604666783);
+  assert_eq!(simhash::hash("Why the lucky stiff"), 1155526875459215761);
+}
+
+#[test]
+fn hamming_distance_test() {
+  assert_eq!(simhash::hamming_distance(0b0000000u64, 0b0000000u64), 0);
+  assert_eq!(simhash::hamming_distance(0b1111111u64, 0b0000000u64), 7);
+  assert_eq!(simhash::hamming_distance(0b0100101u64, 0b1100110u64), 3);
+}
+
+#[test]
+fn hash_similarity_test() {
+  assert_eq!(simhash::hash_similarity(0u64, 0u64), 1.0);
+  assert_eq!(simhash::hash_similarity(!0u64, 0u64), 0.0);
+  assert_eq!(simhash::hash_similarity(!0u32 as u64, 0u64), 0.5);
+}
+
+#[test]
+fn similarity_test() {
+  assert_eq!(simhash::similarity("Stop hammertime", "Stop hammertime"), 1.0);
+  assert!(simhash::similarity("Hocus pocus", "Hocus pocus pilatus pas") > 0.9);
+  assert!(simhash::similarity("Peanut butter", "Strawberry cocktail") < 0.6);
 }
